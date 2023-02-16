0ba4c48c95ffca28e3796dbaeb93eb067d36c6f8
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-02-16 08:45:31 +0530

    variable

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,24 @@
+// Personal Details  
 console.log("Dhanashri Niwdunge");
+console.log("pune");
+console.log(21);
+
+var fullName = "Dhanashri Niwdunge";
+console.log(fullName);
+console.log( fullName);
+
+var city; //Variable declaration
+console.log(city);
+city ="pune"; //variable initialization
+console.log(city);
+city = "surat";//variable update
+console.log(city);
+
+var age = "21"//variable declaration and initialization in same line;
+age = "22";
+age = "23";//update
+console.log(age);
+
+var univercity;//Declare
+univercity = "SGBAU";
+console.log(univercity);
