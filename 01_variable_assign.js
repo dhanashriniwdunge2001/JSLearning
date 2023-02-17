ea20e91ced523d6856b951a9ec0b506b65c27713
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-02-17 23:24:55 +0530

    create new assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+
+console.log("***step 1***");
+var myFullName
+console.log( myFullName);
+
+var myFullName = "Dhanashri Niwdunge";
+console.log(myFullName);
+var myFullName = "Dhanashri Babasaheb Niwdunge";
+console.log(myFullName);
+
+console.log("****step 2****");
+var myLove ="I love only js";
+console.log(myLove);
+
+console.log("****step3****");
+var firstName = "Dhanashri";
+var lastName ="Niwdunge";
+var age = "21";
+var address="pune";
+var pincode ="411046"
+var gender = "female";
+var isMarried ="no";
+console.log(firstName, lastName, age, address, pincode, gender, isMarried );
---
+++ 02_datatypes.js
@@ -0,0 +1,38 @@
+var myName = "Dhanashri Niwdunge";
+var fatherName ="Babasaheb Niwdunge";
+
+var age = 21;
+var weight = 50.8;
+
+var isMarried = false;
+var isLazy = false;
+
+var typeofMyName = typeof myName;
+console.log("Typeof variable myName:",  typeofMyName);
+console.log("typeof variable;", typeof fatherName);
+
+
+var typeofage = typeof age;
+console.log("Typeof variable age:",  typeofage);
+console.log("typeof variable;", typeof weight);
+
+var typeofisMarried = typeof isMarried;
+console.log("Typeof variable isMarried:",  typeofisMarried);
+console.log("typeof variable;", typeof isLazy);
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", graduation);
+
+var city;
+console.log("Value of  city variable is: ", city);
+console.log("typeOf city is: ", city);
+
+console.log("Swapping variables");
+var num1 = 100; // 200
+var num2 = 200; // 100
+
+var temp = num1;
+num1 = num2; 
+num2 = temp;
+console.log(num1, num2);
