00a83ddc40c99d936e4a9e71ece2a27ff47c91cf
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-02-23 08:43:20 +0530

    Update 05_operator.js
    Update 05_operator.js

--- 05_operator.js
+++ 05_operator.js
@@ -30,3 +30,43 @@ var num = 10; // Compound addition +=
  var num4 = 10;
  console.log(num3==num4); // 10==10
  console.log(num3===num4);
+
+
+ //less than equal <=
+ var marks = 70;
+ var result = marks >=60 ? "ALLOW him for Interview": "Don't allow";
+ console.log(result);
+
+ var age = 22;
+ var res = age>= 21? true: false;
+ console.log(res);
+
+ console.log("Even or Odd");
+var myNum = 7;
+//Even - Divide by 2 and reminder is 0
+var result = myNum%2==0 ? "EVEN": "ODD";
+console.log(result );
+
+var num1 = 10;
+var num2 = 12;
+var result = num1 >num2 ? num1 : num2;
+console.log(result);
+
+
+
+function wordLength(word){
+    var wordLength = word.length;
+    var result = wordLength%2==0 ? "EVEN" : "ODD";
+    return result;
+ }
+ var result = wordLength("JavaScript");
+ console.log(`Word "JavaScript" has ${result} length` );
+ // var result = wordLength("Google");
+ console.log(`Word "Google" has ${wordLength("Google Chrome")} length` );
+
+ function maleMarriageEligibility(gender, age, boyName){
+    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are eligible for marriage` : " No Try next time" ;
+    return result;
+ }
+  var result = maleMarriageEligibility("Male", 25, "Billgates");
+  console.log(result);
