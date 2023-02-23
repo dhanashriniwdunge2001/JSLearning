18a5de8e22fd90a76f0e90f2fd7342d23d79d0cf
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-02-23 07:38:32 +0530

    create  new files

---
+++ 04_stringTemplate.js
@@ -0,0 +1,9 @@
+
+
+console.log(`You are "champ" man`);
+
+var firstName = "Dhanashri"
+var lastName = "Niwdunge";
+var city= "pune";
+console.log("My First name ", firstName, "Last name ", lastName, " City ", city);
+console.log(`My first name ${firstName} last name ${lastName} city ${city}`);
---
+++ 05_operator.js
@@ -0,0 +1,32 @@
+console.log(`===Arithmetic Operator===`);
+var num1 = 10;
+var num2 = 2;
+var result = num1 + num2; //Addition
+console.log(`Addition is ${result}`);
+var result = num1 - num2; //Subtraction
+console.log(`Substraction is ${result}`);
+
+var result = num1 * num2;
+console.log(`Multiplication is ${result}`);
+
+var result = num1 ** num2;
+console.log(`Exponential  is ${result}`);
+
+console.log(11/3);
+console.log(10%3);
+
+result = num1 ** 3;
+console.log(`Division is ${result}`);
+
+result = num1 % 3; //modulus
+console.log(`Reminder is ${result}`);
+
+var num = 10; // Compound addition +=
+ num+=20; // num = num + 20
+ console.log(`Compound addition += ${num}`);
+
+ console.log(`Caparison operators `);
+ var num3 = "10";
+ var num4 = 10;
+ console.log(num3==num4); // 10==10
+ console.log(num3===num4);
