875a52a250461cc374562aa50cd0a0023851291e
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-02-28 09:18:07 +0530

    Assignment stringBasic

---
+++ 04_stringBasic_assign.js
@@ -0,0 +1,21 @@
+console.log("*** step 1***");
+
+    var dreamCompany= "Synechron";
+    console.log("My Dream Company is: ", dreamCompany);
+
+    console.log("*** Step 2***");
+    var hobby1 = "Reading";
+    var hobby2 = "Spending time with friends";
+    var hobby3 ="traveling"
+    var hobbies = "Reading, Spending time with friends, traveling";
+    console.log(`My Hobbies is ${hobbies}`);
+console.log(" Total number chars available into this string variable - hobbies ");
+var hobby1Length = hobby1.length;
+console.log("Total no. of char is: ",hobby1Length);
+var hobby2Length = hobby2.length;
+console.log("Total no. of char  is: ",hobby2Length);
+var hobby3Length = hobby3.length;
+console.log("Total no. of char  is: ",hobby3Length);
+
+// console.log(`Total no. of chars is : ${hobby1Length}+${hobby2Length}+${hobby3Length}`);
+console.log(("Sum the Total no. of characters is:42"));
