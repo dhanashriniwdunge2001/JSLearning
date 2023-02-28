f090c1b0e6f7278454ff44751fad4ec014aeb048
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-02-28 09:20:42 +0530

    Update

--- 04_stringBasic_assign.js
+++ 04_stringBasic_assign.js
@@ -11,11 +11,11 @@ console.log("*** step 1***");
     console.log(`My Hobbies is ${hobbies}`);
 console.log(" Total number chars available into this string variable - hobbies ");
 var hobby1Length = hobby1.length;
-console.log("Total no. of char is: ",hobby1Length);
+console.log("Total no. of char in first hobby: ",hobby1Length);
 var hobby2Length = hobby2.length;
-console.log("Total no. of char  is: ",hobby2Length);
+console.log("Total no. of char  in second hobby: ",hobby2Length);
 var hobby3Length = hobby3.length;
-console.log("Total no. of char  is: ",hobby3Length);
+console.log("Total no. of char  in third hobby: ",hobby3Length);
 
 // console.log(`Total no. of chars is : ${hobby1Length}+${hobby2Length}+${hobby3Length}`);
 console.log(("Sum the Total no. of characters is:42"));
