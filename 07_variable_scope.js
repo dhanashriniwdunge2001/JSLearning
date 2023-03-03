881d6306896ab7617466e764f4dba9407d7d83f1
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-03-03 19:59:08 +0530

    update 07_variable_scope.js file
    update 07_variable_scope.js file

--- 07_variable_scope.js
+++ 07_variable_scope.js
@@ -10,10 +10,11 @@ var firstName; // Vaariable re-declaration
 console.log(firstName);
 
 let city ;// Variable declaration
+console.log("Value of city:", city);
 city= "Delhi"; // Variable Initialization
 // let city = "Mumbai"; //Not allowed Vriable re-Declaration
 
-const PI = 3.14;
+const PI =" 3.14";
 //PI =3.56; // update or modify not allowed
 console.log(PI);
 // const PI = 3.1412 ; // Re-declaration of const variable is not allowed
@@ -25,8 +26,39 @@ if(num==10){
 }
 console.log(word);
 
-function sayHi(){
+function sayHi(arg){
 var greet = " Hi Good Morning";
+if (arg){
+    var isEven = true;
+} else {
+   var isEven = false;
 }
-sayHi();
-console.log(greet);
+return isEven;
+}
+//console.log(greet);
+let result = sayHi(10);
+
+console.log(result);
+
+var fullName = "MS Dhoni"
+if (true) {
+    console.log("Hello",age);
+}else{
+    var age = 37;
+
+}
+console.log(age);
+
+
+// 6==> 6*5*4*3*2*1
+//3==> 3* 2 * 1
+
+// function fun (num){
+// let factorial = 1;
+ 
+//   {
+//     factorial = factorial * indexedDB;
+//   }
+// }
+// fun(5);
+// console.log(`factorial is ${ factorial}`);
