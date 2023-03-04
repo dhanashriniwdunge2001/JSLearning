c13aad067df88b924f4b90476fefc5093251a9dd
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-03-04 08:53:37 +0530

    new file

---
+++ revision.js
@@ -0,0 +1,10 @@
+function show(company, arg){
+    console.log(company, arg);
+    return company.concat(arg);
+}
+ let result = show("Codemind","Technology");
+
+  let display = function(arg1, arg2, arg3){
+      return "Success";
+  }
+  let res = display("codemind", 100, "Pune");
