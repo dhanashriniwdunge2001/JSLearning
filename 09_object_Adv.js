cccf2ca140102c752edbd8a710d989a831947868
dhanashriniwdunge2001 <dhanashriniwdunge2001@gmail.com>
2023-03-10 07:58:19 +0530

    new file

---
+++ 09_object_Adv.js
@@ -0,0 +1,98 @@
+
+const employee = {
+    id: 124567,
+    name: "Elon Musk",
+    country : "US",
+    city: "Silicon Valley"
+ }
+ console.log("====in operator====");
+ const isIdAvailable = "id" in employee;
+ console.log(isIdAvailable);
+
+ //const array = [];
+ for (const key in employee) {
+    //console.log(key);
+    if (Object.hasOwnProperty.call(employee, key)) {
+        const element = employee[key];
+        console.log(element);
+        
+    }
+ }
+  const keysOfEmployee = Object.keys(employee);
+  console.log(keysOfEmployee);
+ 
+  const valuesOfEmployee = Object.values(employee)
+  console.log(valuesOfEmployee);
+
+  const entriesOfEmployee = Object.entries(employee);
+  console.log(entriesOfEmployee);
+  console.log(entriesOfEmployee[0]);
+  console.log(entriesOfEmployee[0][1]);
+
+
+
+const player = {
+    fullName: "Virat Kohli",
+    totalCenturies : 46,
+    isMarries: true
+}
+Object.freeze(player);
+player.totalVicket = 120; //Adding new property - Not allowed as object is freezed
+delete player.totalCenturies;// deleting property - Not allowed as object is freezed
+player.fullName = "King Kohli"; // Updating existing property - Not allowed as object is freezed
+
+console.table(player);
+
+const student = {
+    firstName: "Elon",
+    lastName: "Musk",
+    age: 54
+} 
+const address = {
+    country : "US",
+    city : "Silicon Valley",
+    PIN: "QA2345"
+}
+
+// First way 
+const mergedObject = Object.assign({}, student, address);
+console.table(mergedObject);
+
+// Second way 
+const newObject = {};
+Object.assign(newObject, student, address);
+console.table(newObject);
+
+//  Third way
+Object.assign(student, address);
+console.table(student);
+
+//  Fourth way
+Object.assign(address, student);
+console.table(address);
+
+
+
+
+
+
+
+
+
+
+
+//  const sbiBank = {
+// name: "",
+// city: "",
+// showDetails: function(){
+//     //concat
+// }
+//  }
+// sbiBank.showDetails();
+
+//  const yesBank = {
+//     name: " ",
+
+//  }
+
+//Index%2== 0 even Node.
